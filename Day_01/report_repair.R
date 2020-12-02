851abc23d60031472fdbb0da57c59f2278dde6be
kaidolepik <kaidolepik@gmail.com>
2020-12-03 01:25:04 +0200

    Day 01 solutions

---
+++ Day_01/report_repair.R
@@ -0,0 +1,10 @@
+
+product_of_N_expenses_with_sum <- function(expenses, N = 2, expense_sum = 2020) {
+    expense_pairs <- combn(expenses, N)
+    
+    prod(expense_pairs[, colSums(expense_pairs) == expense_sum])
+}
+
+expenses <- scan("/Users/kaidolepik/Desktop/Project/Advent_of_code/Day_01/input.txt")
+product_of_N_expenses_with_sum(expenses, 2) # Day 1.1
+product_of_N_expenses_with_sum(expenses, 3) # Day 1.2
