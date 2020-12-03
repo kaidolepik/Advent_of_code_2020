fe2fb5ccd9310d0354abe9084f04824aeb2c3174
kaidolepik <kaidolepik@gmail.com>
2020-12-03 02:47:06 +0200

    Day 02 solutions

---
+++ Day_02/Password_philosophy.R
@@ -0,0 +1,16 @@
+library(tidyverse)
+
+input <- read.table(text = gsub("(-|: )", " ", readLines("/Users/kaidolepik/Desktop/Project/Advent_of_code/Day_02/input.txt")),
+                    col.names=c("min", "max", "letter", "password"), stringsAsFactors = FALSE)
+
+# Day 1
+input %>%
+    mutate(count = str_count(password, letter)) %>%
+    filter(count >= min & count <= max) %>%
+    nrow()
+
+# Day 2
+input %>%
+    mutate(count = (substr(password, min, min) == letter) + (substr(password, max, max) == letter)) %>%
+    filter(count == 1) %>%
+    nrow()
