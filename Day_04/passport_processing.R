b3f10491b4335b072cefa9c37c7ae766ec15d120
kaidolepik <kaidolepik@gmail.com>
2020-12-04 20:37:58 +0200

    Prettify Day 04

--- Day_04/passport_processing.R
+++ Day_04/passport_processing.R
@@ -14,14 +14,11 @@ sum(rowSums(is.na(input)) == 0 | (rowSums(is.na(input)) == 1 & is.na(input$cid))
 
 # Day 4.2
 input %>%
-    mutate(valid_byr = byr >= 1920 & byr <= 2002,
-           valid_iyr = iyr >= 2010 & iyr <= 2020,
-           valid_eyr = eyr >= 2020 & eyr <= 2030,
-           valid_hgt = ifelse(grepl("cm", hgt), substr(hgt, 1, nchar(hgt)-2) >= 150 & substr(hgt, 1, nchar(hgt)-2) <= 193,
-                              ifelse(grepl("in", hgt), substr(hgt, 1, nchar(hgt)-2) >= 59 & substr(hgt, 1, nchar(hgt)-2) <= 76, NA)),
-           valid_hcl = grepl("#[0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f]$", hcl),
-           valid_ecl = grepl("^(amb|blu|brn|gry|grn|hzl|oth)$", ecl),
-           valid_pid = grepl("^\\d\\d\\d\\d\\d\\d\\d\\d\\d$", pid),
-           valid_cid = TRUE) %>%
-    filter(valid_byr, valid_iyr, valid_eyr, valid_hgt, valid_hcl, valid_ecl, valid_pid, valid_cid) %>%
+    filter(between(byr, 1920, 2002),
+           between(iyr, 2010, 2020),
+           between(eyr, 2020, 2030),
+           grepl("cm", hgt) & between(substr(hgt, 1, nchar(hgt)-2), 150, 193) | grepl("in", hgt) & between(substr(hgt, 1, nchar(hgt)-2), 59, 76),
+           grepl("^#[0-9a-f]{6}$", hcl),
+           grepl("^(amb|blu|brn|gry|grn|hzl|oth)$", ecl),
+           grepl("^\\d{9}$", pid)) %>%
     nrow()
