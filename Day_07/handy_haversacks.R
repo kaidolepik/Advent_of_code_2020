6618651e3991685152ac8a95a5f2a444013f92a6
kaidolepik <kaidolepik@gmail.com>
2020-12-09 14:29:17 +0200

    Minor simplify

--- Day_07/handy_haversacks.R
+++ Day_07/handy_haversacks.R
@@ -1,27 +1,24 @@
 library(tidyverse)
 library(igraph)
 
-input <- data.table::fread("Day_07/input.txt", sep = "\n", header = FALSE) %>%
-    separate(V1, c("from", "to"), sep = " contain ") %>%
+input_graph <- read_delim("Day_07/input.txt", delim = "\n", col_names = "bag") %>%
+    separate(bag, c("from", "to"), sep = " contain ") %>%
     separate_rows(to, sep = ", ") %>%
     mutate(from = sub(" bag.*", "", from),
-           to = sub(" bag.*", "", to))
-
-vertices <- tibble(bag = unique(input$from))
-edges <- input %>%
+           to = sub(" bag.*", "", to)) %>%
     filter(to != "no other") %>%
     separate(to, into = c("weight", "to"), sep = "(?<=\\d) ", convert = TRUE) %>%
-    relocate(weight, .after = to)
-g <- graph_from_data_frame(edges, directed = TRUE, vertices)
+    relocate(weight, .after = to) %>%
+    graph_from_data_frame()
 
 # Day 7.1
-ego_size(g, order = length(V(g)), nodes = "shiny gold", mode = "in", mindist = 1)
+ego_size(input_graph, order = length(V(input_graph)), nodes = "shiny gold", mode = "in", mindist = 1)
 
 # Day 7.2
-paths <- all_simple_paths(g, from = "shiny gold")
+paths <- all_simple_paths(input_graph, from = "shiny gold")
 lapply(paths, function(path) {
-    edges_of_path <- get.edge.ids(g, rep(path, times = c(1, rep(2, length(path)-2), 1)))
-    prod(E(g)$weight[edges_of_path])
+    edges_of_path <- get.edge.ids(input_graph, rep(path, times = c(1, rep(2, length(path)-2), 1)))
+    prod(E(input_graph)$weight[edges_of_path])
 }) %>%
     unlist() %>%
     sum()
