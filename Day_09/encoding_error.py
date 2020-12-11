32ea1a00dea1d2859efaff2cc700a5fa9045c922
kaidolepik <kaidolepik@gmail.com>
2020-12-12 01:44:21 +0200

    Simplify solutions

--- Day_09/encoding_error.py
+++ Day_09/encoding_error.py
@@ -6,15 +6,29 @@ def invalid_number(numbers, k = 25):
         if numbers[i] not in [left + right for left, right in itertools.combinations(numbers[i-k:i], 2)]:
             return numbers[i]
 
-def encryption_weakness(numbers, invalid):
-    for i in range(len(numbers)):
-        cum_sum = np.cumsum(numbers[i:]).tolist()
-        if invalid in cum_sum:
-            return min(numbers[i:i+cum_sum.index(invalid)]) + max(numbers[i:i+cum_sum.index(invalid)])
+def encryption_weakness_short(numbers, invalid):
+    cum_sum = np.cumsum(numbers)
+    ix = np.where(cum_sum - cum_sum[:, np.newaxis] == invalid)
+
+    return min(numbers[ix[0][0]+1:ix[1][0]+1]) + max(numbers[ix[0][0]+1:ix[1][0]+1])
+
+def encryption_weakness_efficient(numbers, invalid):
+    i, j, cum_sum = (0, 0, 0)
+
+    while cum_sum != invalid:
+        if cum_sum < invalid:
+            cum_sum += numbers[j]
+            j += 1
+        else:
+            cum_sum -= numbers[i]
+            i += 1
+
+    return min(numbers[i:j]) + max(numbers[i:j])
 
 
 with open("Day_09/input.txt", "r") as fin:
     numbers = [int(line.strip()) for line in fin]
     
 invalid = invalid_number(numbers); print(invalid) # Day 9.1
-print(encryption_weakness(numbers, invalid)) # Day 9.2
+print(encryption_weakness_short(numbers, invalid)) # Day 9.2 (short code)
+print(encryption_weakness_efficient(numbers, invalid)) # Day 9.2 (efficient code)
