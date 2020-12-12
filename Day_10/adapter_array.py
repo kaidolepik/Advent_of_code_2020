e071174c8b80a529f34e1264b0949d681588b16b
kaidolepik <kaidolepik@gmail.com>
2020-12-12 04:06:51 +0200

    Minor prettify

--- Day_10/adapter_array.py
+++ Day_10/adapter_array.py
@@ -4,8 +4,8 @@ def N_arrangements(diff):
     dynamic = np.ones(len(diff), dtype = np.int64)
 
     for i in range(1, len(dynamic)):
-        prev3_to_i = (3 - sum(diff[i-3:i]) >= 0) if i >= 3 else False
-        prev2_to_i = (3 - sum(diff[i-2:i]) >= 0) if i >= 2 else False
+        prev3_to_i = sum(diff[i-3:i]) <= 3 if i >= 3 else False
+        prev2_to_i = sum(diff[i-2:i]) <= 3 if i >= 2 else False
 
         dynamic[i] = dynamic[i-1] + dynamic[i-2]*prev2_to_i + dynamic[i-3]*prev3_to_i
 
